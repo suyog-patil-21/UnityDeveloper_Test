170a662c65bfc966511320be149d3ecd6a87d0aa
Suyog Patil <suyog0057@gmail.com>
2024-07-26 17:31:31 +0530

    add: animator for the movement of the player with blendTree

--- UnityDeveloper_Test/Assets/Scripts/Player Script/PlayerController.cs
+++ UnityDeveloper_Test/Assets/Scripts/Player Script/PlayerController.cs
@@ -10,10 +10,13 @@ public class PlayerController : MonoBehaviour
     float rotationSpeed = 500f;
 
     Quaternion targetRotation;
+
+
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+     animator = GetComponent<Animator>();    
     }
 
     // Update is called once per frame
@@ -27,12 +30,14 @@ public class PlayerController : MonoBehaviour
 
     var moveInput = (new Vector3(h,0,v)).normalized;
 
-    float moveAmount = Mathf.Abs(h) + Mathf.Abs(v);
+    float moveAmount = Mathf.Clamp01(Mathf.Abs(h) + Mathf.Abs(v));
 
     if(moveAmount > 0) {
         transform.position += moveInput * moveSpeed * Time.deltaTime;
         targetRotation= Quaternion.LookRotation(moveInput);
     }
     transform.rotation = Quaternion.RotateTowards(transform.rotation,targetRotation,rotationSpeed * Time.deltaTime);
+
+    animator.SetFloat("moveAmount",moveAmount,0.2f,Time.deltaTime);
     }
 }
