c2133a596a703d1e91daf1ad8892a7be76929c81
Suyog Patil <suyog0057@gmail.com>
2024-07-26 19:02:30 +0530

    add: CameraController & fix playerMovement with mouse

--- UnityDeveloper_Test/Assets/Scripts/CameraController.cs
+++ UnityDeveloper_Test/Assets/Scripts/CameraController.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] 
+     Transform followTarget;
+
+    [SerializeField] 
+    float distance = 5;
+
+    [SerializeField] 
+    float rotationSpeed = 5f;
+
+    [SerializeField] 
+    float minVerticalAngle = -10;
+
+    [SerializeField] 
+    float maxVerticalAngle = 45;
+
+    [SerializeField] 
+    Vector2 framingOffset;
+
+    [SerializeField] 
+    bool invertX;
+    [SerializeField] 
+    bool invertY;
+
+    float invertXVal;
+    float invertYVal;
+
+    float rotationX;
+    float rotationY;
+
+    void Start() {
+     Cursor.visible = false;
+     Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        invertXVal = (invertX)? -1 : 1;
+        invertYVal = (invertY)? -1 : 1;
+
+        rotationX += Input.GetAxis("Mouse Y") * invertYVal* rotationSpeed;
+        rotationX = Mathf.Clamp(rotationX, minVerticalAngle,maxVerticalAngle);
+        rotationY += Input.GetAxis("Mouse X") * invertXVal * rotationSpeed;
+
+        var targetRotation = Quaternion.Euler(rotationX,rotationY,0);
+
+        // Camera Focus Region Area 
+        var focusPosition = followTarget.position + new Vector3(framingOffset.x,framingOffset.y);
+
+        transform.position = focusPosition - targetRotation * new Vector3(0,0,distance);
+        transform.rotation = targetRotation;
         
     }
+
+    public Quaternion PlannerRotation => Quaternion.Euler(0,rotationY,0);
 }
--- UnityDeveloper_Test/Assets/Scripts/Player Script/PlayerController.cs
+++ UnityDeveloper_Test/Assets/Scripts/Player Script/PlayerController.cs
@@ -11,30 +11,30 @@ public class PlayerController : MonoBehaviour
 
     Quaternion targetRotation;
 
-
+    CameraController cameraController;
     Animator animator;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        cameraController =  Camera.main.GetComponent<CameraController>();
      animator = GetComponent<Animator>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-    float invertAxisDirection = -1;
-    float horizontalAxis = Input.GetAxis("Horizontal");        
-    float verticalAxis = Input.GetAxis("Vertical");        
-    float h = horizontalAxis * invertAxisDirection;
-    float v = verticalAxis * invertAxisDirection;
+    float h = Input.GetAxis("Horizontal"); 
+    float v = Input.GetAxis("Vertical");        
 
     var moveInput = (new Vector3(h,0,v)).normalized;
 
     float moveAmount = Mathf.Clamp01(Mathf.Abs(h) + Mathf.Abs(v));
 
+    var moveDir = cameraController.PlannerRotation * moveInput;
+
     if(moveAmount > 0) {
-        transform.position += moveInput * moveSpeed * Time.deltaTime;
-        targetRotation= Quaternion.LookRotation(moveInput);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        targetRotation= Quaternion.LookRotation(moveDir);
     }
     transform.rotation = Quaternion.RotateTowards(transform.rotation,targetRotation,rotationSpeed * Time.deltaTime);
 
